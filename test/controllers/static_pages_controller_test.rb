fcc6c9ab2899b8480f19254342bbe7dab12d2d4a
mau5atron <gabrielbetancourt96@outlook.com>
2018-04-11 01:56:05 -0700

    Refactored title for each static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,30 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+	# DONT REPEAT YOURSELF - lets write a method that displays the 
+	# ruby on rails tut sample app 
+	# and interpolate that binch 😩👌
+
+	def setup
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title" "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title",  "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success 
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  	assert_select "title", "About | #{@base_title}"
   end
 
 end
