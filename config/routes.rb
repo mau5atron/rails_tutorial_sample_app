acad35b97d2a630b0968dbf3e8f2c172268ead12
mau5atron <gabrielbetancourt96@outlook.com>
2018-04-14 01:44:40 -0700

    Added route helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
 
 	root 'static_pages#home'
 
-  get 'static_pages/home'
+  get '/help' => 'static_pages#help'
+  # or get '/help', to: 'static_pages#help'
+  # this also creates a new variable called help_path
+  # examples: help_path, about_path, x_path
 
-  get 'static_pages/help'
+  get '/about' => 'static_pages#about'
 
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  get '/contact' => 'static_pages#contact'
 
   # root 'application#hello'
 end
